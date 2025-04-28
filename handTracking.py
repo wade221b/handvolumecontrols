6a3915b8a27373332138e291e5540ae76f2b4bc6
Shreyas Wade <Shreyas@dhcp-172-21-46-196.wlan.uni-hamburg.de.local>
2025-04-28 17:34:01 +0200

    points on hand in original image

--- handTracking.py
+++ handTracking.py
@@ -1,4 +1,3 @@
-from unittest import result
 import cv2
 import time
 import mediapipe as mp
@@ -9,12 +8,15 @@ mpHands = mp.solutions.hands
 
 hands = mpHands.Hands(False)
 #static_image_mode => true : whole time detects a hand. makes code slow as continuously tracking
-# false : detects and if found > some confidence level, tracks
-#         max_num_hands=2,
-#    model_complexity=1,
-#    min_detection_confidence=0.5,
-#    min_tracking_confidence=0.5)
+#                     false : detects and if found > some confidence level, tracks
+# max_num_hands=2,
+# model_complexity=1,
+# min_detection_confidence=0.5,
+# min_tracking_confidence=0.5)
 #only uses RGB images. hence the image from camera has to first be converted
+
+mpDraw = mp.solutions.drawing_utils 
+#function to draw line between the 21 points.
 
 while True:
     success, img = cap.read()
@@ -22,6 +24,12 @@ while True:
     #to send RGB image to the hands. for that first need to convert 
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  #the image from camera has to first be converted
     results = hands.process(imgRGB)
+
+    print(results.multi_hand_landmarks)
+
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks: #draw lines for each hand ditected
+            mpDraw.draw_landmarks(img, handLms) #because we are displaying on the original image, we pass this and not the RGB image
 
     cv2.imshow("Image", img)
     # cv2.waitKey(1) #basic code to run a webcam-
