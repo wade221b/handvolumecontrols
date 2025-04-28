8873636d23b3ade123bd8dd0671cd88b59678d18
Shreyas Wade <Shreyas@dhcp-172-21-46-196.wlan.uni-hamburg.de.local>
2025-04-28 16:25:54 +0200

    basic code to close camera, and track hand

--- handTracking.py
+++ handTracking.py
@@ -1,11 +1,32 @@
+from unittest import result
 import cv2
 import time
 import mediapipe as mp
 
 cap = cv2.VideoCapture(1)
 
+mpHands = mp.solutions.hands
+
+hands = mpHands.Hands(False)
+#static_image_mode => true : whole time detects a hand. makes code slow as continuously tracking
+# false : detects and if found > some confidence level, tracks
+#         max_num_hands=2,
+#    model_complexity=1,
+#    min_detection_confidence=0.5,
+#    min_tracking_confidence=0.5)
+#only uses RGB images. hence the image from camera has to first be converted
+
 while True:
     success, img = cap.read()
 
+    #to send RGB image to the hands. for that first need to convert 
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  #the image from camera has to first be converted
+    results = hands.process(imgRGB)
+
     cv2.imshow("Image", img)
-    cv2.waitKey(1) #basic code to run a webcam
+    # cv2.waitKey(1) #basic code to run a webcam-
+
+    if cv2.waitKey(1) & 0xFF==ord("q"):
+        break #https://stackoverflow.com/questions/46821936/open-cv-close-camera
+
+cv2.destroyAllWindows()
